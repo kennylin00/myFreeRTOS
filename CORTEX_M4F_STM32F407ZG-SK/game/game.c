e4013bb9ad45f54dfec37a2a0046186f1fbd2303
kennylin00 <kennylin1322@gmail.com>
2014-08-20 22:39:08 +0800

    test

---
+++ CORTEX_M4F_STM32F407ZG-SK/game/game.c
@@ -0,0 +1,412 @@
+#include "game.h"
+#include "main.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MOVE 1
+#define BANG 2
+#define POWERBALL_GREEN 3
+#define POWERBALL_RED 4
+
+
+//Player1
+int16_t player1X = 10;
+int16_t player1Y = 10;
+uint16_t player1W = 60;
+uint16_t player1H = 10;
+uint8_t player1IsReversed = 1;
+
+//Player2
+int16_t player2X = LCD_PIXEL_WIDTH - 20;
+int16_t player2Y = LCD_PIXEL_HEIGHT - 20;
+uint16_t player2W = 60;
+uint16_t player2H = 10;
+uint8_t player2IsReversed = 0;
+
+//Ball
+uint16_t ballSize = 5;
+int16_t ballX = ( LCD_PIXEL_WIDTH - 5 ) / 2;
+int16_t ballY = ( LCD_PIXEL_HEIGHT - 5 ) / 2;
+int16_t volatile ballVX = 5;
+int16_t volatile ballVY = 5;
+uint8_t ballIsRun = 0;
+
+//Mode
+uint8_t demoMode = 0;
+
+//PowerBall
+int PowerBallX = 0;
+int PowerBallY = 0;
+static volatile int CrushFlag = MOVE;
+int PowerBallType = POWERBALL_RED;
+extern int RefreshSpeed;
+
+void
+BallReset()
+{
+	ballX = ( LCD_PIXEL_WIDTH - 5 ) / 2;
+	ballY = ( LCD_PIXEL_HEIGHT - 5 ) / 2;
+
+		ballVX = 5;
+		ballVY = 5;
+	ballIsRun = 1;
+}
+
+void
+GAME_EventHandler1()
+{
+	if( STM_EVAL_PBGetState( BUTTON_USER ) ){
+
+		player1IsReversed = 0;
+
+		while( STM_EVAL_PBGetState( BUTTON_USER ) );
+
+		player1IsReversed = 1;
+	}
+}
+
+void
+GAME_EventHandler2()
+{
+	if( IOE_TP_GetState()->TouchDetected ){
+
+		player2IsReversed = 1;
+
+		while( IOE_TP_GetState()->TouchDetected );
+
+		player2IsReversed = 0;
+	}
+}
+
+void
+GAME_EventHandler3()
+{
+	if( ballIsRun == 0 ){
+		BallReset();
+	}
+}
+
+void
+GAME_PowerBall()
+{
+	int i,j=0;
+	int Type = (rand()%2)+1;
+	if(Type == 1)
+		PowerBallType = POWERBALL_GREEN;
+	else
+		PowerBallType = POWERBALL_RED;
+
+	//PowerBallX = (rand()% 230)+ 50;
+	//PowerBallY = (rand()% 200)+ 0;
+	PowerBallX = 110;//(rand()% 230)+ 50; //110
+	PowerBallY = 160;//(rand()% 200)+ 50; //160
+
+
+	while(j<5){
+		if(CrushFlag == BANG) {
+			CrushFlag = MOVE;
+			break;
+		}
+		if(PowerBallType == POWERBALL_GREEN)
+			LCD_SetTextColor( LCD_COLOR_GREEN );
+		else
+			LCD_SetTextColor( LCD_COLOR_RED );
+
+		LCD_DrawFullRect( PowerBallX, PowerBallY, ballSize, ballSize );
+
+
+		for(i=0;i<1000000;i++) {
+		 __asm volatile("nop");	
+		 __asm volatile("nop");	
+		 __asm volatile("nop");	
+		 __asm volatile("nop");	
+		 __asm volatile("nop");	
+		}
+
+		LCD_SetTextColor( LCD_COLOR_BLACK );
+		LCD_DrawFullRect( PowerBallX, PowerBallY, ballSize, ballSize );
+		j++;
+
+	}
+	
+
+}
+
+int DetectCrush(int Src, int Targ)
+{
+	if(((Targ - Src) >=-4) && ((Targ -Src) <=4))
+		return 1;
+	else
+		return 0;
+}
+
+void
+GAME_Update()
+{
+
+	int i;
+	//Player1
+	LCD_SetTextColor( LCD_COLOR_BLACK );
+	LCD_DrawFullRect( player1X, player1Y, player1W, player1H );
+	LCD_DrawFullRect( player2X, player2Y, player2W, player2H );
+
+	if( demoMode == 0 ){
+
+		if( player1IsReversed )
+			player1X -= 5;
+		else
+			player1X += 5;
+
+		if( player1X <= 0 )
+			player1X = 0;
+		else if( player1X + player1W >= LCD_PIXEL_WIDTH )
+			player1X = LCD_PIXEL_WIDTH - player1W;
+
+		//Player2
+		if( player2IsReversed )
+			player2X -= 5;
+		else
+			player2X += 5;
+
+		if( player2X <= 0 )
+			player2X = 0;
+		else if( player2X + player2W >= LCD_PIXEL_WIDTH )
+			player2X = LCD_PIXEL_WIDTH - player2W;
+
+		//Ball
+		if( ballIsRun == 1 ){
+
+			LCD_SetTextColor( LCD_COLOR_BLACK );
+			LCD_DrawFullRect( ballX, ballY, ballSize, ballSize );
+
+			if(DetectCrush(ballX, PowerBallX) && (DetectCrush(ballY, PowerBallY))){
+				RefreshSpeed=5;
+				LEDLightOff(4);
+				LEDLightOff(3);
+				CrushFlag = BANG;
+				LCD_SetTextColor( LCD_COLOR_BLACK );
+				LCD_DrawFullRect( PowerBallX, PowerBallY, ballSize, ballSize );
+		
+				LCD_SetTextColor( LCD_COLOR_YELLOW );
+				LCD_DrawCircle( PowerBallX +2, PowerBallY +2, 5 );
+				
+				if(PowerBallType == POWERBALL_GREEN){
+					RefreshSpeed= 10;
+					LEDLightOn(3);
+					LEDLightOff(4);
+				}
+				else{
+					RefreshSpeed= 5;
+					LEDLightOn(4);
+					LEDLightOff(3);
+				}
+
+				LCD_DrawCircle( PowerBallX+2, PowerBallY +2, 5 );
+				for(i=0;i<500000;i++) {
+					 __asm volatile("nop");
+					 __asm volatile("nop");	
+					 __asm volatile("nop");	
+				}	
+				LCD_SetTextColor( LCD_COLOR_BLACK );
+				LCD_DrawCircle( PowerBallX+2, PowerBallY +2, 5 );
+				
+			}
+
+
+			//Touch wall
+			ballX += ballVX;
+			if( ballX <= 0 ){
+				ballX = 0;
+				ballVX *= -1;
+			}
+			else if( ballX + ballSize >= LCD_PIXEL_WIDTH ){
+				ballX = LCD_PIXEL_WIDTH - ballSize;
+				ballVX *= -1;
+			}
+
+			//PONG!
+			ballY += ballVY;
+			if( ballY + ballSize >= player2Y ){
+				if( ballX + ballSize >= player2X && ballX <= player2X + player2W ){
+					if( ballX - ballSize <= player2Y + player2W/4 ){
+						ballVY =-3;
+						ballVX =-7;
+					}
+					else if( ballX >= player2Y + player2W - player2W/4 ){
+						ballVY =-3;
+						ballVX = 7;
+					}
+					else if( ballX + ballSize < player2Y + player2W/2 ){
+						ballVY =-7;
+						ballVX =-3;
+					}
+					else if( ballX > player2Y + player2W/2 ){
+						ballVY =-7;
+						ballVX = 3;
+					}
+					else{
+						ballVY =-9;
+						ballVX = 0;
+					}
+				}
+				else
+					BallReset();
+			}
+
+			if( ballY <= player1Y + player1H ){
+
+					if( ballX + ballSize >= player1X && ballX <= player1X + player1W ){
+						if( ballX - ballSize <= player1Y + player1W/4 ){
+							ballVY = 3;
+							ballVX =-7;
+						}
+						else if( ballX >= player1Y + player1W - player1W/4 ){
+							ballVY = 3;
+							ballVX = 7;
+						}
+						else if( ballX + ballSize < player1Y + player1W/2 ){
+							ballVY = 7;
+							ballVX =-3;
+						}
+						else if( ballX > player1Y + player1W/2 ){
+							ballVY = 7;
+							ballVX = 3;
+						}
+						else{
+							ballVY = 9;
+							ballVX = 0;
+						}
+					}
+					else
+						BallReset();
+				}
+			}
+		}
+		else{	//if demoMode == 1
+
+			//Player1 move
+			if( ballVY < 0 ){
+				if( player1X + player1W/2 < ballX + ballSize/2 ){
+					player1X += 8;
+					player2X += 2;
+				}
+				else{
+					player1X -= 8;
+					player2X -= 2;
+				}
+			}
+
+			//Player2 move
+			if( ballVY > 0 ){
+				if( player2X + player2W/2 < ballX + ballSize/2 ){
+					player1X += 2;
+					player2X += 8;
+				}
+				else{
+					player1X -= 2;
+					player2X -= 8;
+				}
+
+			}
+
+			if( player1X <= 0 )
+				player1X = 0;
+			else if( player1X + player1W >= LCD_PIXEL_WIDTH )
+				player1X = LCD_PIXEL_WIDTH - player1W;
+
+			if( player2X <= 0 )
+				player2X = 0;
+			else if( player2X + player2W >= LCD_PIXEL_WIDTH )
+				player2X = LCD_PIXEL_WIDTH - player2W;
+
+
+			//Ball
+			if( ballIsRun == 1 ){
+
+				LCD_SetTextColor( LCD_COLOR_BLACK );
+				LCD_DrawFullRect( ballX, ballY, ballSize, ballSize );
+
+				//Touch wall
+				ballX += ballVX;
+				if( ballX <= 0 ){
+					ballX = 0;
+					ballVX *= -1;
+				}
+				else if( ballX + ballSize >= LCD_PIXEL_WIDTH ){
+					ballX = LCD_PIXEL_WIDTH - ballSize;
+					ballVX *= -1;
+				}
+
+				//PONG!
+				ballY += ballVY;
+				if( ballY + ballSize >= player2Y ){
+					if( ballX + ballSize >= player2X && ballX <= player2X + player2W ){
+					if( ballX - ballSize <= player2Y + player2W/4 ){
+						ballVY =-3;
+						ballVX =-7;
+					}
+					else if( ballX >= player2Y + player2W - player2W/4 ){
+						ballVY =-3;
+						ballVX = 7;
+					}
+					else if( ballX + ballSize < player2Y + player2W/2 ){
+						ballVY =-7;
+						ballVX =-3;
+					}
+					else if( ballX > player2Y + player2W/2 ){
+						ballVY =-7;
+						ballVX = 3;
+					}
+					else{
+						ballVY =-9;
+						ballVX = 0;
+					}
+				}
+				else
+					BallReset();
+			}
+
+			if( ballY <= player1Y + player1H ){
+				if( ballX + ballSize >= player1X && ballX <= player1X + player1W ){
+					if( ballX - ballSize <= player1Y + player1W/4 ){
+						ballVY = 3;
+						ballVX =-7;
+					}
+					else if( ballX >= player1Y + player1W - player1W/4 ){
+						ballVY = 3;
+						ballVX = 7;
+					}
+					else if( ballX + ballSize < player1Y + player1W/2 ){
+						ballVY = 7;
+						ballVX =-3;
+					}
+					else if( ballX > player1Y + player1W/2 ){
+						ballVY = 7;
+						ballVX = 3;
+					}
+					else{
+						ballVY = 9;
+						ballVX = 0;
+					}
+				}
+				else
+					BallReset();
+			}
+		}
+	}
+}
+
+	void
+GAME_Render()
+{
+	LCD_SetTextColor( LCD_COLOR_WHITE );
+	LCD_DrawFullRect( player1X, player1Y, player1W, player1H );
+	LCD_DrawFullRect( player2X, player2Y, player2W, player2H );
+	LCD_DrawFullRect( ballX, ballY, ballSize, ballSize );
+	LCD_DrawLine( 10, LCD_PIXEL_HEIGHT / 2, LCD_PIXEL_WIDTH - 20, LCD_DIR_HORIZONTAL );
+}
---
+++ CORTEX_M4F_STM32F407ZG-SK/main.c
@@ -0,0 +1,166 @@
+/**
+  ******************************************************************************
+  * @file    Template/main.c 
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    20-September-2013
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "game.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/** @addtogroup Template
+  * @{
+  */ 
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+extern uint8_t demoMode;
+
+int RefreshSpeed =10;
+
+void
+prvInit()
+{
+	//LCD init
+	LCD_Init();
+	IOE_Config();
+	LTDC_Cmd( ENABLE );
+
+	LCD_LayerInit();
+	LCD_SetLayer( LCD_FOREGROUND_LAYER );
+	LCD_Clear( LCD_COLOR_BLACK );
+	LCD_SetTextColor( LCD_COLOR_WHITE );
+
+	//Button
+	STM_EVAL_PBInit( BUTTON_USER, BUTTON_MODE_GPIO );
+
+	//LED
+	STM_EVAL_LEDInit( LED3 );
+}
+
+void LEDLightInit()
+{
+	STM_EVAL_LEDInit( LED3 );
+	STM_EVAL_LEDInit( LED4 );
+
+}
+void LEDLightOn(int i)
+{
+	switch(i){
+		case 3:
+			STM_EVAL_LEDOn( LED3 );			
+		break;
+		case 4:
+			STM_EVAL_LEDOn( LED4 );			
+		break;
+		default:
+		break;
+	}
+}
+
+
+void LEDLightOff(int i)
+{
+	switch(i){
+		case 3:
+			STM_EVAL_LEDOff( LED3 );			
+		break;
+		case 4:
+			STM_EVAL_LEDOff( LED4 );			
+		break;
+		default:
+		break;
+	}
+}
+
+
+static void GameEventTask1( void *pvParameters )
+{
+	while( 1 ){
+		GAME_EventHandler1();
+	}
+}
+
+static void GameEventTask2( void *pvParameters )
+{
+	while( 1 ){
+		GAME_EventHandler2();
+	}
+}
+
+static void GameEventTask3( void *pvParameters )
+{
+	while( 1 ){
+		GAME_EventHandler3();
+	}
+}
+
+static void GameTask( void *pvParameters )
+{
+	while( 1 ){
+		GAME_Update();
+		GAME_Render();
+		vTaskDelay( RefreshSpeed );
+	}
+}
+
+
+static void GamePowerBallTask( void *pvParameters )
+{
+	while( 1 ){
+		GAME_PowerBall();
+		vTaskDelay( 30 );
+	}
+}
+
+//Main Function
+int main(void)
+{
+	prvInit();
+
+	LEDLightInit();
+	LEDLightOff(3);
+	LEDLightOff(4);
+
+
+	if( STM_EVAL_PBGetState( BUTTON_USER ) )
+		demoMode = 1;
+
+	xTaskCreate( GameTask, (signed char*) "GameTask", 128, NULL, tskIDLE_PRIORITY + 1, NULL );
+	xTaskCreate( GameEventTask1, (signed char*) "GameEventTask1", 128, NULL, tskIDLE_PRIORITY + 1, NULL );
+	xTaskCreate( GameEventTask2, (signed char*) "GameEventTask2", 128, NULL, tskIDLE_PRIORITY + 1, NULL );
+	xTaskCreate( GameEventTask3, (signed char*) "GameEventTask3", 128, NULL, tskIDLE_PRIORITY + 1, NULL );
+	xTaskCreate( GamePowerBallTask, (signed char*) "GamePowerBallTask", 128, NULL, tskIDLE_PRIORITY + 1, NULL );
+
+	//Call Scheduler
+	vTaskStartScheduler();
+}
